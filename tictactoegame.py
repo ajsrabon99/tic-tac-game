e98c0212fded24e1a489ccfdce36ef13992549cf
Ashrafuzzaman <ashrafuzzamansrabon@gmail.com>
2024-11-19 21:20:24 +0600

    add restart button

--- tictactoegame.py
+++ tictactoegame.py
@@ -11,16 +11,19 @@ def start_game():
     label.grid()  
     
     # Show Privacy Policy button at the bottom of the game
-    privacy_button.grid(row=7, column=0, columnspan=3, pady=10)  
+    privacy_button.grid(row=8, column=0, columnspan=3, pady=10)  
+    exit_button.grid(row=9, column=0, columnspan=3, pady=(0, 20))  # Show Exit button at the bottom of the game
 
 def check_winner():
+    global winner
     for combo in [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]:
         if buttons[combo[0]]["text"] == buttons[combo[1]]["text"] == buttons[combo[2]]["text"] != "":
             buttons[combo[0]].config(bg="honeydew2")
             buttons[combo[1]].config(bg="honeydew2")
             buttons[combo[2]].config(bg="honeydew2")
+            winner = True
             messagebox.showinfo("Tic-Tac-Toe", f"Player {buttons[combo[0]]['text']} wins!")
-            root.quit()
+            show_restart_button()  # Show Restart button when game ends
             return True  
     return False  
 
@@ -28,7 +31,7 @@ def check_tie():
     if all(button["text"] != "" for button in buttons):
         if not check_winner():  
             messagebox.showinfo("Tic-Tac-Toe", "Match Draw!")
-            root.quit()
+            show_restart_button()  # Show Restart button when game ends
 
 def button_click(index):
     if buttons[index]["text"] == "" and not winner:
@@ -47,66 +50,28 @@ def toggle_player():
     label.config(text=f"Player {current_player}'s turn")
 
 def show_privacy_policy():
-    privacy_policy_text = """Privacy Policy
-AJ Srabon built the Tic-Tac-Toe game as a Free game. This SERVICE is provided by AJ Srabon at no cost and is intended for use as is.
-
-This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Service.
-
-If you choose to use my Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that I collect is used for providing and improving the Service. I will not use or share your information with anyone except as described in this Privacy Policy.
-
-The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Tic-Tac-Toe unless otherwise defined in this Privacy Policy.
-
-Information Collection and Use
-
-For a better experience, while using our Service, I may require you to provide us with certain personally identifiable information. The information that I request will be retained on your device and is not collected by me in any way.
-
-The game does use third party services that may collect information used to identify you.
-
-Link to privacy policy of third party service providers used by the game
-
-Google Play Services
-Log Data
-
-I want to inform you that whenever you use my Service, in a case of an error in the game I collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the game when utilizing my Service, the time and date of your use of the Service, and other statistics.
-
-Cookies
-
-Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.
-
-This Service does not use these “cookies” explicitly. However, the game may use third party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.
-
-Service Providers
-
-I may employ third-party companies and individuals due to the following reasons:
-
-To facilitate our Service;
-To provide the Service on our behalf;
-To perform Service-related services; or
-To assist us in analyzing how our Service is used.
-I want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.
-
-Security
-
-I value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and I cannot guarantee its absolute security.
-
-Links to Other Sites
-
-This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by me. Therefore, I strongly advise you to review the Privacy Policy of these websites. I have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
-
-Children’s Privacy
-
-These Services do not address anyone under the age of 13. I do not knowingly collect personally identifiable information from children under 13. In the case I discover that a child under 13 has provided me with personal information, I immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact me so that I will be able to do necessary actions.
-
-Changes to This Privacy Policy
-
-I may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.
-
-Contact Us
-
-If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at ashrafuzzamansrabon@gmail.com
-"""
-
+    privacy_policy_text = """AJ Srabon built the Tic-Tac-Toe game as a Free game. This SERVICE is provided by AJ Srabon at no cost and is intended for use as is.
+    """
     messagebox.showinfo("Privacy Policy", privacy_policy_text)
+
+def show_restart_button():
+    restart_button.grid(row=7, column=0, columnspan=3, pady=10)  # Show Restart button
+
+def restart_game():
+    global current_player, winner
+    current_player = "X"
+    winner = False
+    label.config(text=f"Player {current_player}'s turn")
+    
+    # Reset all buttons
+    for button in buttons:
+        button.config(text="", bg="SystemButtonFace")
+    
+    # Hide the Restart button
+    restart_button.grid_remove()
+
+def exit_game():
+    root.quit()
 
 root = tk.Tk()
 root.title("Tic-Tac-Toe")
@@ -139,5 +104,15 @@ label.grid(row=6, column=0, columnspan=3)
 label.grid_remove()  
 
 privacy_button = tk.Button(center_frame, text="Privacy Policy", font=("normal", 12), command=show_privacy_policy)
+
+# Add Restart Button (initially hidden)
+restart_button = tk.Button(center_frame, text="Restart", font=("normal", 16), command=restart_game)
+restart_button.grid(row=7, column=0, columnspan=3, pady=10)
+restart_button.grid_remove()
+
+# Add Exit Button
+exit_button = tk.Button(center_frame, text="Exit", font=("normal", 12), command=exit_game)
+exit_button.grid(row=9, column=0, columnspan=3, pady=(0, 20))
+exit_button.grid_remove()  # Hide it initially, it will be shown after starting the game
 
 root.mainloop()
